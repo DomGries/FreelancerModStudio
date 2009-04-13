a2d9b1cb8212c247c97ef44807c890251f30435a
stfx <stfx@hotmail.de>
2009-04-13 06:00:36 +0000

    Various undocumented changes

---
+++ FreelancerModStudio/frmOptions.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace FreelancerModStudio
+{
+    partial class frmOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOptions));
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.AccessibleDescription = null;
+            this.propertyGrid.AccessibleName = null;
+            resources.ApplyResources(this.propertyGrid, "propertyGrid");
+            this.propertyGrid.BackgroundImage = null;
+            this.propertyGrid.Font = null;
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.PropertySort = System.Windows.Forms.PropertySort.Categorized;
+            this.propertyGrid.ToolbarVisible = false;
+            // 
+            // frmOptions
+            // 
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
+            this.Controls.Add(this.propertyGrid);
+            this.Font = null;
+            this.Icon = null;
+            this.Name = "frmOptions";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid;
+    }
+}
---
+++ FreelancerModStudio/frmOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FreelancerModStudio
+{
+    public partial class frmOptions : Form
+    {
+        public frmOptions()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.LogoIcon;
+            this.propertyGrid.SelectedObject = Helper.Settings.Data.Data.General;
+            this.propertyGrid.ExpandAllGridItems();
+        }
+    }
+}

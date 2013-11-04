c0d1605861b26592b67e529a8b88d4ac4bcb98c3
stfx <stfx@hotmail.de>
2013-11-04 13:21:33 +0000

    Publish 1.1.2

--- FreelancerModStudio/Properties/AssemblyInfo.cs
+++ FreelancerModStudio/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d88a7221-eaf7-4b5b-a285-a71912942382")]
-[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.2")]
